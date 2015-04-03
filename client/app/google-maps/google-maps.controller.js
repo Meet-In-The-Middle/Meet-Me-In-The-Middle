192f9d8f86d7ee678167b7e966ad80154426672b
Jordan Genung <jordanmgenung@gmail.com>
2015-04-03 12:11:37 -0500

    implemented geolocation func/added midpoint script

--- client/app/google-maps/google-maps.controller.js
+++ client/app/google-maps/google-maps.controller.js
@@ -17,7 +17,15 @@ angular.module('meetMeInTheMiddleApp')
 
         $scope.map = { control: {}, center: { latitude: 40.1451, longitude: -99.6680 }, zoom: 4, refresh: {}};
 
-        $scope.options = {scrollwheel: false};
+        $scope.options = {scrollwheel: false, scaleControl: true};
+
+        $scope.windowOptions = {
+            visible: false
+        };
+
+        $scope.onClick = function() {
+            $scope.windowOptions.visible = !$scope.windowOptions.visible;
+        };
 
         $scope.marker = {
           id: 0,
@@ -25,13 +33,13 @@ angular.module('meetMeInTheMiddleApp')
             latitude: 52.47491894326404,
             longitude: -1.8684210293371217
           },
+          show: false,
           options: { draggable: true },
           events: {
             dragend: function (marker, eventName, args) {
-                $scope.test1 = "asdfasdf"
               $scope.marker.options = {
                   draggable: true,
-                  labelContent: "lat: " + $scope.marker.coords.latitude + ' ' + 'lon: ' + $scope.marker.coords.longitude,
+                  labelContent: "lat: " + $scope.marker.coords.latitude + " " + "lon: " + $scope.marker.coords.longitude,
                   labelAnchor: "100 0",
                   labelClass: "marker-labels"
               };
@@ -41,10 +49,10 @@ angular.module('meetMeInTheMiddleApp')
 
         var events = {
           places_changed: function (searchBox) {
-            console.log('asdf');
             var place = searchBox.getPlaces();
+            console.log(place);
             if (!place || place == 'undefined' || place.length == 0) {
-                console.log('no place data :(');
+                console.log('no place data');
                 return;
             }
             $scope.map = {
@@ -56,14 +64,54 @@ angular.module('meetMeInTheMiddleApp')
             };
 
             $scope.marker = {
+                placeID: place[0].id,
+                name: place[0].name,
+                address: place[0].formatted_address,
                 id: 0,
                 coords: {
                     latitude: place[0].geometry.location.lat(),
                     longitude: place[0].geometry.location.lng()
-                }
+                },
+                options: {
+                  draggable: true,
+                  labelContent: "lat: " + $scope.marker.coords.latitude + " " + "lon: " + $scope.marker.coords.longitude,
+                  labelAnchor: "100 0",
+                  labelClass: "marker-labels"
+              },
+              show: true
             };
           }
         }
+
+      uiGmapGoogleMapApi.then(function(maps) {
+        $scope.resolved = true;
+        $scope.googleVersion = maps.version;
+        maps.visualRefresh = true;        
+      });
+
+       
+
+        $scope.locator = function(){
+          navigator.geolocation.getCurrentPosition(
+          function(pos) {
+            $scope.map = { control: {}, center: { latitude: pos.coords.latitude, longitude: pos.coords.longitude }, zoom: 18};
+            $scope.marker = {
+                id: 0,
+                coords: {
+                    latitude: pos.coords.latitude,
+                    longitude: pos.coords.longitude
+                }
+
+            };
+            $scope.$apply();
+           }, 
+          function(error) {
+            alert('Unable to get location: ' + error.message);
+          }
+        );
+        }  
+        
+
 
         // Connect to socket when the user places pin on the map
         var socket = io();
@@ -83,11 +131,25 @@ angular.module('meetMeInTheMiddleApp')
 
         $scope.searchbox = { template:'searchbox.tpl.html', events:events};
 
-        uiGmapGoogleMapApi.then(function(maps) {
-          $scope.resolved = true;
-          $scope.googleVersion = maps.version;
-          maps.visualRefresh = true;
-       });
+
+
+
+    }])
+
+    .controller('controlCtrl', function ($scope) {
+        $scope.controlText = 'Find Me';
+        $scope.danger = false;
+        $scope.controlClick = function () {
+            $scope.danger = !$scope.danger;
+            alert('custom control clicked!');
+        };
+    });
+
+
+
+
+
+
 
   //         $scope.routePoints.start = $scope.map.routes.start[0];
   // $scope.routePoints.end = $scope.map.routes.end[0];
@@ -111,30 +173,6 @@ angular.module('meetMeInTheMiddleApp')
   //   });
   //   return;
   // };
-
-    }]);
-
-
-// .controller('MapsCtrl', ['$scope', '$q', '$http', 'uiGmapGoogleMapApi', 
-
-//   function($scope, $q, $http, uiGmapGoogleMapApi) {
-//     $scope.resolved = false;
-//     $scope.map = {center: {latitude: 40.1451, longitude: -99.6680 }, zoom: 4 };
-//     $scope.options = {scrollwheel: false};
-
-//     var events = {
-//       places_changed: function (searchBox) {}
-//     }
-//     $scope.searchbox = { template:'searchbox.tpl.html', events:events};
-    
-//     uiGmapGoogleMapApi.then(function(maps) {
-//           $scope.resolved = true;
-//           $scope.googleVersion = maps.version;
-//           maps.visualRefresh = true;
-//     });
-
-//   }])
-
 
     // $scope.refreshMap = function () {
     //   //optional param if you want to refresh you can pass null undefined or false or empty arg

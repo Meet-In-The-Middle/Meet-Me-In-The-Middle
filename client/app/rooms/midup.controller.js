38978ce0b359022747bfcb4f7b23e2b2baebdbcb
Jonah Nisenson <jsnisenson@gmail.com>
2015-04-13 16:12:22 -0500

    (feat) chat functionality built out 1. on refresh or entering midUp, 100 most recent messages pulled from DB 2. submit message working 3. Chat box automatically scrolls to bottom using 'angular-glue-scroll' (bower install)(feat) chat functionality built out 1. on refresh or entering midUp, 100 most recent messages pulled from DB 2. submit message working 3. Chat box automatically scrolls to bottom using 'angular-glue-scroll' (bower install)(feat) chat functionality built out 1. on refresh or entering midUp, 100 most recent messages pulled from DB 2. submit message working 3. Chat box automatically scrolls to bottom using 'angular-glue-scroll' (bower install)(feat) chat functionality built out 1. on refresh or entering midUp, 100 most recent messages pulled from DB 2. submit message working 3. Chat box automatically scrolls to bottom using 'angular-glue-scroll' (bower install)(feat) chat functionality built out 1. on refresh or entering midUp, 100 most recent messages pulled from DB 2. submit message working 3. Chat box automatically scrolls to bottom using 'angular-glue-scroll' (bower install)(feat) chat functionality built out 1. on refresh or entering midUp, 100 most recent messages pulled from DB 2. submit message working 3. Chat box automatically scrolls to bottom using 'angular-glue-scroll' (bower install)(feat) chat functionality built out 1. on refresh or entering midUp, 100 most recent messages pulled from DB 2. submit message working 3. Chat box automatically scrolls to bottom using 'angular-glue-scroll' (bower install)(feat) chat functionality built out 1. on refresh or entering midUp, 100 most recent messages pulled from DB 2. submit message working 3. Chat box automatically scrolls to bottom using 'angular-glue-scroll' (bower install)(feat) chat functionality built out 1. on refresh or entering midUp, 100 most recent messages pulled from DB 2. submit message working 3. Chat box automatically scrolls to bottom using 'angular-glue-scroll' (bower install)

--- client/app/rooms/midup.controller.js
+++ client/app/rooms/midup.controller.js
@@ -8,17 +8,19 @@ angular.module('meetMeInTheMiddleApp')
     var userId = user._id;
     var username = user.name;
     var socket = SocketFactory.socket;
-    console.log('user  ', user);
+    //console.log('user  ', user);
     $scope.user = {};
+    $scope.messages = [];
     var url = $location.$$path.split('/');
     var roomId = url[url.length - 1];
-    console.log('roomId is ', roomId);
+    //console.log('roomId is ', roomId);
 
     /**
      * init called when page loads
      */
     $scope.init = function() {
       addUserToRoom();
+      loadChatMessages();
     };
     /**
      * send user and room information to server to be stored in DB (i.e. add this user to room in DB)
@@ -41,11 +43,17 @@ angular.module('meetMeInTheMiddleApp')
         info: 'How awesome',
         active: true
       };
-
       socket.emit('join-room', userRoomObj);
-
-
     };
+
+    var loadChatMessages = function() {
+      socket.on('chat-messages', function(data) {
+        $scope.messages = data;
+        var chatBox = angular.element('.chat-box')
+        chatBox.scrollTop = chatBox.scrollHeight;
+      })
+    };
+
     /**
      * send chat message by emitting to roomId (which means only that room will receive message)
      */
@@ -53,15 +61,14 @@ angular.module('meetMeInTheMiddleApp')
       var message = $scope.userMessage;
       var userId = Auth.getCurrentUser()._id;
       var username = Auth.getCurrentUser().name;
-      console.log('sendChat called ', $scope.userMessage);
       socket.emit(roomId, userId, username, message);
       $scope.userMessage = "";
     };
 
-    socket.on('serversend', function(username, message) {
-      console.log(444444, username, message);
-      var newMessage = '<div><span class="chatUserName">'+username+': </span><span class="message">' +message+ '</span>';
-      angular.element('.chat-box').append(newMessage);
+    socket.on('server-chat-response', function(messageObj) {
+      $scope.$apply(function() {
+        $scope.messages = $scope.messages.concat(messageObj);
+      });
     });
 
 

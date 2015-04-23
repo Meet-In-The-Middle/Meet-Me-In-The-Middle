465a808bbc7badc55d2ca29630f60bdcbe78eef5
Jonah Nisenson <jsnisenson@gmail.com>
2015-04-23 18:44:35 -0500

    (fix) add comment blocks to head of functions on server and client code

--- client/app/rooms/midup.controller.js
+++ client/app/rooms/midup.controller.js
@@ -2,89 +2,101 @@
 
 angular.module('meetMeInTheMiddleApp')
 
-.controller('midUpCtrl', ['$scope', '$timeout', '$http', '$location','Auth', 'MainFactory', 'SocketFactory',
-  function ($scope, $timeout, $http, $location, Auth, MainFactory, SocketFactory) {
+  .controller('midUpCtrl', ['$scope', '$timeout', '$http', '$location','Auth', 'MainFactory', 'SocketFactory',
+    function ($scope, $timeout, $http, $location, Auth, MainFactory, SocketFactory) {
 
-    //Populate select box with the places nearby options
-    $scope.places_Nearby = MainFactory.places_Nearby;
-    //Holder for items selected
-    $scope.possiblePlaces = [];
-    $scope.selectedPlace = [];
-    $scope.selectedPlaces = [];
-    var user = Auth.getCurrentUser();
-    var userId = user._id;
-    var username = user.name;
-    var socket = SocketFactory.socket;
-    console.log('socket is ', socket);
-    $scope.user = {};
-    $scope.messages = [];
-    var url = $location.$$path.split('/');
-    var roomId = url[url.length - 1];
-    $scope.roomNonExistest = false;
+      //Populate select box with the places nearby options
+      $scope.places_Nearby = MainFactory.places_Nearby;
+      //Holder for items selected
+      $scope.possiblePlaces = [];
+      $scope.selectedPlace = [];
+      $scope.selectedPlaces = [];
+      var user = Auth.getCurrentUser();
+      var userId = user._id;
+      var username = user.name;
+      var socket = SocketFactory.socket;
+      console.log('socket is ', socket);
+      $scope.user = {};
+      $scope.messages = [];
+      var url = $location.$$path.split('/');
+      var roomId = url[url.length - 1];
+      $scope.roomNonExistest = false;
+
+      /**
+       *
+       * @param string
+       */
+      $scope.addPlace = function(string){
+        if($scope.selectedPlaces.indexOf(string) === -1){
+          console.log('about to push');
+          $scope.selectedPlaces.push(string);
+        }
+        console.log('running');
+      };
+      /**
+       *
+       */
+      $scope.removePlace = function(){
+        var tempArr = $scope.selectedPlaces;
+        var i = tempArr.indexOf($scope.selectedPlace.length-1);
+        $scope.selectedPlaces = tempArr.splice(i);
+        //console.log('about to push');
+        //$scope.selectedPlaces.push(string);
+        // }
+        console.log('running');
+      };
 
 
-    $scope.addPlace = function(string){
-      if($scope.selectedPlaces.indexOf(string) === -1){
-        console.log('about to push');
-        $scope.selectedPlaces.push(string);
-      }
-      console.log('running');
-    };
-    $scope.removePlace = function(){
-      var tempArr = $scope.selectedPlaces;
-      var i = tempArr.indexOf($scope.selectedPlace.length-1);
-      $scope.selectedPlaces = tempArr.splice(i);
-      //console.log('about to push');
-      //$scope.selectedPlaces.push(string);
-    // }
-      console.log('running');
-    };
 
+      /**
+       * init called when page loads
+       */
+      $scope.init = function() {
+        //addUserToRoom();  //commented out while we test socket.io vs http request when joining midUp
+        loadChatMessages();
+      };
+      /**
+       *
+       */
+      var loadChatMessages = function() {
+        socket.on('chat-messages', function(data) {
+          $scope.messages = data;
+          var chatBox = angular.element('.chat-box')
+          chatBox.scrollTop = chatBox.scrollHeight;
+        })
+      };
 
-
-    /**
-     * init called when page loads
-     */
-    $scope.init = function() {
-      //addUserToRoom();  //commented out while we test socket.io vs http request when joining midUp
-      loadChatMessages();
-    };
-
-    var loadChatMessages = function() {
-      socket.on('chat-messages', function(data) {
-        $scope.messages = data;
-        var chatBox = angular.element('.chat-box')
-        chatBox.scrollTop = chatBox.scrollHeight;
-      })
-    };
-
-    /**
-     * send chat message by emitting to roomId (which means only that room will receive message)
-     */
-    $scope.sendChat = function() {
-      var message = $scope.userMessage;
-      var userId = Auth.getCurrentUser()._id;
-      var username = Auth.getCurrentUser().name;
-      socket.emit(roomId, userId, username, message);
-      $scope.userMessage = "";
-    };
-
-    socket.on('server-chat-response', function(messageObj) {
-      $scope.$apply(function() {
-        $scope.messages = $scope.messages.concat(messageObj);
-      });
-    });
-
-    socket.on('error-msg', function(errMsg) {
-      console.log('errMsg is ', errMsg);
-      if( errMsg === 'midup does not exist or has been deleted') {
+      /**
+       * send chat message by emitting to roomId (which means only that room will receive message)
+       */
+      $scope.sendChat = function() {
+        var message = $scope.userMessage;
+        var userId = Auth.getCurrentUser()._id;
+        var username = Auth.getCurrentUser().name;
+        socket.emit(roomId, userId, username, message);
+        $scope.userMessage = "";
+      };
+      /**
+       *
+       */
+      socket.on('server-chat-response', function(messageObj) {
         $scope.$apply(function() {
-          $scope.roomNonExistest = true;
+          $scope.messages = $scope.messages.concat(messageObj);
         });
-        $timeout(function() {
-          $location.path('/mymidups');
-        }, 5000);
-      }
-    });
+      });
+      /**
+       *
+       */
+      socket.on('error-msg', function(errMsg) {
+        console.log('errMsg is ', errMsg);
+        if( errMsg === 'midup does not exist or has been deleted') {
+          $scope.$apply(function() {
+            $scope.roomNonExistest = true;
+          });
+          $timeout(function() {
+            $location.path('/mymidups');
+          }, 5000);
+        }
+      });
 
-  }]);
+    }]);
--- client/app/rooms/rooms.controller.js
+++ client/app/rooms/rooms.controller.js
@@ -1,184 +1,216 @@
 'use strict';
 
 angular.module('meetMeInTheMiddleApp')
-.controller('roomsCtrl', ['$scope', '$http', '$window', '$location', '$q', 'Auth', 'MainFactory', 'SocketFactory',
-  function ($scope, $http, $window, $location, $q,  Auth, MainFactory, SocketFactory) {
+  .controller('roomsCtrl', ['$scope', '$http', '$window', '$location', '$q', 'Auth', 'MainFactory', 'SocketFactory',
+    function ($scope, $http, $window, $location, $q,  Auth, MainFactory, SocketFactory) {
 
-    //profile controller methods
-    var user = Auth.getCurrentUser();
-    console.log('user is ', user);
-    var userId = user._id;
-    console.log('userId is ', userId);
-    $scope.user = {};
-    $scope.inviteEmails = {};
-    $scope.clickedFlag = false;
-    $scope.remove = false;
-    var url = $location.$$path.split('/');
-    var roomId = url[url.length - 1];
-    var socket = io();
-    /**
+      //profile controller methods
+      var user = Auth.getCurrentUser();
+      console.log('user is ', user);
+      var userId = user._id;
+      console.log('userId is ', userId);
+      $scope.user = {};
+      $scope.inviteEmails = {};
+      $scope.clickedFlag = false;
+      $scope.remove = false;
+      var url = $location.$$path.split('/');
+      var roomId = url[url.length - 1];
+      var socket = io();
+      /**
        * initial page on load to show rooms user belongs to
        * and to set $scope elements for editable text box to create new room (midup)
-        */
-    $scope.init = function () {
-      $scope.user.name = user.name;
-      editableTextBox();
-      emailInviteInit();
-    };
-
-    //load rooms the user is in, ie query DB for users rooms (rooms)
-    var asyncGetRooms = function(Id) {
-      return $q(function(resolve, reject) {
-        setTimeout(function() {
-          console.log('got into setTimeout');
-          if( Id ) {
-            console.log('Id is ', Id);
-            resolve(Id);
-          }
-        }, 500);
-      });
-    };
-
-    var promise = asyncGetRooms(Auth.getCurrentUser());
-    promise.then(function() {
-      console.log('got into promise.then');
-      getRooms();
-    });
-    var getRooms = function() {
-      console.log('Auth.getCurrentUser()._id is ', Auth.getCurrentUser()._id);
-      MainFactory.getRoomsForUser(Auth.getCurrentUser()._id, function(rooms) {
-        console.log('rooms for this user are ', rooms);
-        $scope.rooms = rooms;
-      });
-      //return an array of rooms
-    };
-    /**
-     * Set $scope elements for create room button to show input to get new room name
-     */
-    var editableTextBox = function() {
-      $scope.editorEnabled = false;
-      $scope.enableEditor = function() {
-        $scope.editorEnabled = true;
-        $scope.editableText = $scope.user.about;
-      };
-      $scope.disableEditor = function() {
-        $scope.editorEnabled = false;
-      };
-    };
-    /**
-     * User creates new room which will be sent to DB and then re-populate rooms list
-     */
-    $scope.createRoom = function() {
-      var userRoomObj = {
-        roomId: roomId,
-        name: $scope.editableText,
-        user: {
-          userId: user._id,
-          name: user.name,
-          coords: {
-            latitude: "",
-            longitude: ""
-          },
-          owner: true,
-          imageUrl: ""
-        },
-        info: $scope.roomInfo || '',
-        active: true
+       */
+      $scope.init = function () {
+        $scope.user.name = user.name;
+        editableTextBox();
+        emailInviteInit();
       };
 
-
-      MainFactory.createRoom(userRoomObj, function() {
-        console.log('userRoomObj is ', userRoomObj);
+      //load rooms the user is in, ie query DB for users rooms (rooms)
+      /**
+       *
+       * @param Id
+       * @returns {*}
+       */
+      var asyncGetRooms = function(Id) {
+        return $q(function(resolve, reject) {
+          setTimeout(function() {
+            console.log('got into setTimeout');
+            if( Id ) {
+              console.log('Id is ', Id);
+              resolve(Id);
+            }
+          }, 500);
+        });
+      };
+      /**
+       *
+       * @type {*}
+       */
+      var promise = asyncGetRooms(Auth.getCurrentUser());
+      promise.then(function() {
+        console.log('got into promise.then');
         getRooms();
       });
-      $scope.disableEditor();
-      $scope.remove = false;
-    };
-
-    /////////Handle email invitations//////////
-    var emailInviteInit = function() {
-      $scope.inviteEnabled = false;
-    };
-
-    $scope.enableInvite = function(roomId) {
-      console.log('roomId ', roomId);
-      $scope.inviteEnabled = function(room) {
-        return room === roomId? true: false;
+      var getRooms = function() {
+        console.log('Auth.getCurrentUser()._id is ', Auth.getCurrentUser()._id);
+        MainFactory.getRoomsForUser(Auth.getCurrentUser()._id, function(rooms) {
+          console.log('rooms for this user are ', rooms);
+          $scope.rooms = rooms;
+        });
+        //return an array of rooms
       };
-      //$scope.inviteEmails = $scope.emails;
-    };
+      /**
+       * Set $scope elements for create room button to show input to get new room name
+       */
+      var editableTextBox = function() {
+        $scope.editorEnabled = false;
+        $scope.enableEditor = function() {
+          $scope.editorEnabled = true;
+          $scope.editableText = $scope.user.about;
+        };
+        $scope.disableEditor = function() {
+          $scope.editorEnabled = false;
+        };
+      };
+      /**
+       * User creates new room which will be sent to DB and then re-populate rooms list
+       */
+      $scope.createRoom = function() {
+        var userRoomObj = {
+          roomId: roomId,
+          name: $scope.editableText,
+          user: {
+            userId: user._id,
+            name: user.name,
+            coords: {
+              latitude: "",
+              longitude: ""
+            },
+            owner: true,
+            imageUrl: ""
+          },
+          info: $scope.roomInfo || '',
+          active: true
+        };
 
-    $scope.disableInvite = function() {
-      $scope.inviteEnabled = false;
-    };
 
-    $scope.clicked = function() {
-      console.log('clicked was called');
-      $scope.clickedFlag = true;
-    }
-
-    $scope.keyUp = function() {
-      $scope.clickedFlag = false;
-    };
-    console.log('socket is ', socket);
-    $scope.sendEmailInvites = function(roomId, roomName) {
-      console.log('sendEmailInvites called', $scope.inviteEmails);
-      var pattern = /\s*[,;]\s*/;
-      var emails = $scope.inviteEmails[roomId].split(pattern);
-      //console.log('emails is ', emails);
-      socket.emit('email-invites', emails, roomId, user.name, roomName);
-      socket.on('email-invites-reply', function(message) {
-        console.log('email-invite-reply ', message);
-      });
-    };
-
-    var midUpIndex;
-    $scope.deleteMidup = function(roomId, index) {
-      midUpIndex = index;
-      console.log('midUpIndex is ', midUpIndex);
-      var result = $window.confirm('Are you certain you want to DELETE this MidUp? Changes cannot be recovered.\n\n' +
-      'Click OK to Delete or Cancel');
-      if( result === true ) {
-        socket.emit('delete-midup', roomId, userId);
-      }
-    };
-
-    $scope.leaveMidup = function(roomId, index) {
-      midUpIndex = index;
-      console.log('midUpIndex is ', midUpIndex);
-      var result = $window.confirm('Are you certain you want to LEAVE this MidUp?\n\n' +
-      'Click OK to Delete or Cancel');
-      if( result === true ) {
-        socket.emit('leave-midup', roomId, userId);
-      }
-    };
-
-    socket.on('delete-midup-reply', function(data) {
-      console.log('data is ', data);
-      if( data === 'Midup was removed' ) {
-        $scope.$apply(function() {
-          $scope.remove = true;
+        MainFactory.createRoom(userRoomObj, function() {
+          console.log('userRoomObj is ', userRoomObj);
+          getRooms();
         });
-      } else if( !!data ) {
-        $scope.$apply(function() {
-          $scope.rooms = data;
-        });
-      }
-    });
+        $scope.disableEditor();
+        $scope.remove = false;
+      };
 
-    socket.on('leave-midup-reply', function(data) {
-      console.log('data is ', data);
-      if( !!data ) {
-        $scope.$apply(function() {
-          if( data.length === 0 ) { $scope.remove = true; }
-          else {
-            $scope.remove = false;
+      /**
+       *
+       * */
+      var emailInviteInit = function() {
+        $scope.inviteEnabled = false;
+      };
+      /**
+       *
+       * */
+      $scope.enableInvite = function(roomId) {
+        console.log('roomId ', roomId);
+        $scope.inviteEnabled = function(room) {
+          return room === roomId? true: false;
+        };
+        //$scope.inviteEmails = $scope.emails;
+      };
+      /**
+       *
+       * */
+      $scope.disableInvite = function() {
+        $scope.inviteEnabled = false;
+      };
+      /**
+       *
+       * */
+      $scope.clicked = function() {
+        console.log('clicked was called');
+        $scope.clickedFlag = true;
+      }
+      /**
+       * */
+      $scope.keyUp = function() {
+        $scope.clickedFlag = false;
+      };
+      /**
+       *
+       * */
+      $scope.sendEmailInvites = function(roomId, roomName) {
+        console.log('sendEmailInvites called', $scope.inviteEmails);
+        var pattern = /\s*[,;]\s*/;
+        var emails = $scope.inviteEmails[roomId].split(pattern);
+        //console.log('emails is ', emails);
+        socket.emit('email-invites', emails, roomId, user.name, roomName);
+        socket.on('email-invites-reply', function(message) {
+          console.log('email-invite-reply ', message);
+        });
+      };
+
+      var midUpIndex;
+      /**
+       *
+       * @param roomId
+       * @param index
+       */
+      $scope.deleteMidup = function(roomId, index) {
+        midUpIndex = index;
+        console.log('midUpIndex is ', midUpIndex);
+        var result = $window.confirm('Are you certain you want to DELETE this MidUp? Changes cannot be recovered.\n\n' +
+        'Click OK to Delete or Cancel');
+        if( result === true ) {
+          socket.emit('delete-midup', roomId, userId);
+        }
+      };
+      /**
+       *
+       * @param roomId
+       * @param index
+       */
+      $scope.leaveMidup = function(roomId, index) {
+        midUpIndex = index;
+        console.log('midUpIndex is ', midUpIndex);
+        var result = $window.confirm('Are you certain you want to LEAVE this MidUp?\n\n' +
+        'Click OK to Delete or Cancel');
+        if( result === true ) {
+          socket.emit('leave-midup', roomId, userId);
+        }
+      };
+      /**
+       *
+       */
+      socket.on('delete-midup-reply', function(data) {
+        console.log('data is ', data);
+        if( data === 'Midup was removed' ) {
+          $scope.$apply(function() {
+            $scope.remove = true;
+          });
+        } else if( !!data ) {
+          $scope.$apply(function() {
             $scope.rooms = data;
-          }
-        });
-      }
-    });
+          });
+        }
+      });
+      /**
+       *
+       */
+      socket.on('leave-midup-reply', function(data) {
+        console.log('data is ', data);
+        if( !!data ) {
+          $scope.$apply(function() {
+            if( data.length === 0 ) { $scope.remove = true; }
+            else {
+              $scope.remove = false;
+              $scope.rooms = data;
+            }
+          });
+        }
+      });
 
-  }]);
+    }]);
 
 
